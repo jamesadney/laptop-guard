2bd34a951b42a478b43ce447eb689b4c22422760
James Adney <jfadney@gmail.com>
2011-03-26 02:18:38 -0700

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='Laptop Guard',
+      version='0.6',
+      description='A laptop alarm gtk application',
+      author='James Adney',
+      author_email='jfadney@gmail.com',
+      packages=['laptopguard'],
+      scripts=['alarm-service', 'laptop-guard'],
+      data_files=[('laptop-guard/ui', ['ui/alarm.glade']),
+                  ('laptop-guard/media', ['media/caralarm.ogg', 'media/locked.png', 
+                             'media/system-lock-screen.svg']),
+                  ('/usr/share/doc/laptop-guard', ['COPYING', 'README.rst', 
+                                                   'TODO'])]
+     )
--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rfv /usr/share/laptop-guard
-rm -rfv /usr/share/doc/laptop-guard
-rm -fv /usr/bin/laptop-guard
-rm -fv /usr/share/dbus-1/services/org.theftalarm.Alarm.service
