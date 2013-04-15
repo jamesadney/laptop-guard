80a14183efa79714b310950381e99d24a012c591
James Adney <jfadney@gmail.com>
2013-04-15 13:43:56 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Laptop Guard',
-      version='0.6.3',
+      version='0.6.4',
       description='A laptop alarm gtk application',
       author='James Adney',
       author_email='jfadney@gmail.com',
